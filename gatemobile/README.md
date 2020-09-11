c37b931078dadcbb3ffcee1e5ca3eb4477f2615c
Arek Cios <arek123113@gmail.com>
2020-09-11 23:16:52 +0200

    Make it work

--- gatemobile/README.md
+++ gatemobile/README.md
@@ -1,10 +1,8 @@
 # gatemobile
 
-A new Flutter project.
+:iphone: Mobile application for automatic entry gate remote sytstem, written in Flutter
 
 ## Getting Started
-
-This project is a starting point for a Flutter application.
 
 A few resources to get you started if this is your first Flutter project:
 
--- gatemobile/lib/main.dart
+++ gatemobile/lib/main.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' show jsonEncode;
+
+Future<http.Response> changeStatus() {
+  return http.post(
+    'https://stormy-badlands-80104.herokuapp.com/api/status',
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(<String, bool>{
+      'IsClosed': true,
+      'ShouldBeClosed': false,
+    }),
+  );
+}
 
 void main() {
   runApp(MaterialApp(
@@ -16,11 +31,11 @@ void main() {
         padding: EdgeInsets.all(8.0),
         splashColor: Colors.blueAccent,
         onPressed: () {
-          /*...*/
+          changeStatus();
         },
         child: Text(
           "Wyślij sygnał",
-          style: TextStyle(fontSize: 20.0),
+          style: TextStyle(fontSize: 45.0),
         ),
       )),
     ),
